1e88f8daddecc8256c56973fd7d6220aef9abc25
Sachin Fulari <sachinfulari529@gmail.com>
2026-08-03 23:13:01 +0530

    Implemented Step 3 for Contact Page

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,8 @@ export default defineConfig({
         services: page('html/services.html'),
         testimonials: page('html/testimonials.html'),
         scheduleStep1: page('html/schedule-step-1.html'),
-        scheduleStep2: page('html/schedule-step-2.html')
+        scheduleStep2: page('html/schedule-step-2.html'),
+        scheduleStep3: page('html/schedule-step-3.html')
       }
     }
   }
